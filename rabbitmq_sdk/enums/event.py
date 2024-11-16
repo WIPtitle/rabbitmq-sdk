0261b353758648da39b3bc2434c6258eeb7020db
galvagnimatteo <galvagni.matteo@protonmail.com>
2024-11-16 14:16:07 +0100

    feat: add alarm waiting event

--- rabbitmq_sdk/enums/event.py
+++ rabbitmq_sdk/enums/event.py
@@ -4,7 +4,8 @@ from enum import Enum, auto
 class Event(Enum):
     CAMERA_ALARM = auto(),
     REED_ALARM = auto(),
-    ALARM_STOPPED = auto()
+    ALARM_STOPPED = auto(),
+    ALARM_WAITING = auto(),
 
     def get_name(self):
         return self.name
---
+++ rabbitmq_sdk/event/impl/devices_manager/alarm_waiting.py
@@ -0,0 +1,28 @@
+from rabbitmq_sdk.enums.event import Event
+from rabbitmq_sdk.enums.service import Service
+from rabbitmq_sdk.event.base_event import BaseEvent
+
+
+class AlarmWaiting(BaseEvent):
+    def __init__(self, timestamp: int):
+        super().__init__(Service.DEVICES_MANAGER, Event.ALARM_WAITING)
+        self.timestamp = timestamp
+
+    def to_dict(self):
+        event_dict = {
+            "timestamp": self.timestamp
+        }
+        return event_dict
+
+    @classmethod
+    def from_dict(cls, data):
+        timestamp = int(data["timestamp"])
+        return cls(timestamp)
+
+    @property
+    def timestamp(self):
+        return self._timestamp
+
+    @timestamp.setter
+    def timestamp(self, value: int):
+        self._timestamp = value
